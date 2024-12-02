a1d7d5135b854ee9033d04ec03fd7ba3f71d32aa
Nícolas Deves <nicolas.deves@universo.univates.br>
2024-12-01 22:28:41 -0300

    Feat: add in memory repository for tests

---
+++ src/repositories/in-memory/in-memory-user-repository.ts
@@ -0,0 +1,33 @@
+import { Prisma, User } from "@prisma/client";
+import { UserRepository } from "../user-repository";
+
+export class InMemoryUserRepository implements UserRepository{
+
+    public items: User[] = []
+
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+
+        const user = {
+            id: 1,
+            name: data.name,
+            email: data.email,
+            password_hash: data.password_hash,
+            created_at: new Date()
+        }
+
+        this.items.push(user)
+
+        return user
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = this.items.find(user => user.email === email)
+
+        if (!user) {
+            return null
+        }
+
+        return user
+    }
+
+}
